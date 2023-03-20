191aa3caeb7c196be195908292cff0bf513fd422
877wrangler <877wrangler@gmail.com>
2023-03-19 23:45:25 -0400

    Add env variables

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+dotenv.config();
+
+mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_URL}`
+    );
 
 app.listen(3001, () => console.log("Server has started..."));
